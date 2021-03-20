9bb89d0604a10c3ab035ca013f8344fe126587aa
Evan McPheron <evan.mcpheron@icloud.com>
2021-03-19 18:41:46 -0700

    working on models and routes

--- server.js
+++ server.js
@@ -12,15 +12,6 @@ app.use('/api/imageUpload', require('./routes/imageUpload'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/email', require('./routes/email'));
 
-// SERVE static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
 });
