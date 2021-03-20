cd12282ec0ab9031961083ae63ebe9c36f04ab86
Evan McPheron <evan.mcpheron@icloud.com>
2021-03-19 20:06:32 -0700

    first commit

--- server.js
+++ server.js
@@ -14,12 +14,6 @@ app.use('/api/imageUpload', require('./routes/imageUpload'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/email', require('./routes/email'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App listening on port ${process.env.PORT}!`);
 });
