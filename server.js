0acb421edf2056c31fc6763d8a76b114bb200641
Evan McPheron <evan.mcpheron@icloud.com>
2021-03-19 19:51:35 -0700

    working on models and routes

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+app.get('/', (req, res) => res.send('API Running'));
+
 app.use('/api/imageUpload', require('./routes/imageUpload'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/email', require('./routes/email'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App listening on port ${process.env.PORT}!`);
